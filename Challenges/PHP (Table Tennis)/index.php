4a03591b06347fb83c1b9104746b9322b1e9391d
Finn Middleton <92194155+FinnMidd@users.noreply.github.com>
2024-02-26 23:27:35 +1100

    Updated index.php
    Added CSS styling
    Added JS style manipulation
    Added PHP hyperlinks

--- Challenges/PHP (Table Tennis)/index.php
+++ Challenges/PHP (Table Tennis)/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <?php
@@ -15,7 +16,7 @@
             }
         }
     }
-    
+
     function getPlayerGames($id) {
         // Define variables & arrays
         $filename = "snippet_data.json";
@@ -24,7 +25,6 @@
         $players_array = $decode_data["players"];
         $games_array = $decode_data["games"];
         $player_games = [];
-
 
         // Check ID is valid & update status
         foreach ($players_array as $player) {
@@ -38,9 +38,9 @@
 
         // Check status
         if ($valid_status == false) {
-            echo "ID not found";
+            echo "<p>ID not found</p>";
         } else {
-            echo "Player #$id (" . getPlayerName($id, $players_array) . ") found.<br><br>"; //! testing purposes only
+            echo "<h4>Player #$id (" . getPlayerName($id, $players_array) . ") found.</h4>";
             // Loop through games array
             foreach ($games_array as $game) {
                 // Check if ID is found in any game stats
@@ -52,40 +52,49 @@
 
             // If no games found
             if (empty($player_games)) {
-                echo "This player has no recorded games";
+                echo "<p>This player has no recorded games</p>";
             } else {
                 // If games found
-                // for each game echo stats
+                // for each game echo stats within a div
                 foreach ($player_games as $game) {
+                    // Echo container & score
+                    echo "<div class='game-container'>";
+                    echo "<p><span style='font-weight: bold; '>Tournament Match</span></p>";
+                    echo "<p>Score: " . $game['winnerScore'] . " - " . $game['loserScore'] . "</p>";
+
                     // Highlight player outcome
                     if ($game['winnerId'] == $id) {
-                        echo "Tournament Match<br>";
-                        echo "Score: " . $game['winnerScore'] . " - " . $game['loserScore'] . "<br>";
-                        echo "<span style='color: limegreen; font-weight: bold;'>Winner: " . " " . getPlayerName($game['winnerId'], $players_array) . "</span><br>";
-                        echo "Loser: " . getPlayerName($game['loserId'], $players_array) . "<br>";
-                        echo "<br>";
+                        echo "<p><span class='winner'>Winner: " . " " . getPlayerName($game['winnerId'], $players_array) . "</span></p>";
+                        echo "<p>Loser: <a class='player-link' href='?id=" . $game['loserId'] . "'>" . getPlayerName($game['loserId'], $players_array) . "</a></p>";
                     } else {
-                        echo "Tournament Match<br>";
-                        echo "Score: " . $game['winnerScore'] . " - " . $game['loserScore'] . "<br>";
-                        echo "Winner: " . " " . getPlayerName($game['winnerId'], $players_array) . "<br>";
-                        echo "<span style='color: red; font-weight: bold;'>Loser: " . getPlayerName($game['loserId'], $players_array) . "</span><br>";
-                        echo "<br>";
+                        echo "<p>Winner: <a class='player-link' href='?id=" . $game['winnerId'] . "'>" . getPlayerName($game['winnerId'], $players_array) . "</a></p>";
+                        echo "<p><span class='loser'>Loser: " . getPlayerName($game['loserId'], $players_array) . "</span></p>";
                     }
+                    echo "</div>";
                 }
+                echo "<br>";
             }
         }   
     }
 
-    // !Test
-    getPlayerGames('2130');
-
-    // Use playerGames array to echo stats
-
-    // getPlayerGames(christian's id 1234)
-
-    // lookup games array, get all games containing winner & loser as 1234
-    // for each game returned, log tournament match details
+    //! Test
+    // Check if page is loaded via hyperlink
+    if (isset($_GET['id'])) {
+        // Get ID from url
+        $id = $_GET['id'];
+        // Run function
+        getPlayerGames($id);
+    } else {
+        //* Manual input here
+        getPlayerGames(2127);
+    }
 
     ?>
+
+    <footer>
+        <p><a href="https://github.com/FinnMidd/Jala" class="me">&copy; Finn Middleton 2024</a></p>
+    </footer>
+
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ Challenges/PHP (Table Tennis)/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Define class & delay variables
+    var containers = document.querySelectorAll(".game-container");
+    var delay = 0.1;
+
+    // Set animation delay for game containers
+    containers.forEach(function(container) {
+        container.style.animationDelay = delay + "s";
+        // Increase delay for the next element
+        delay += 0.1;
+    });
+
+    // Reveal footer after each elements has loaded
+    var footer = document.querySelector("footer");
+    footer.style.animationDelay = delay + "s";
+});
